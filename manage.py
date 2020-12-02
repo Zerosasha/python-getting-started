074a0043ea39cfcd743b4bac91f8657004034822
Zerosasha <75369400+Zerosasha@users.noreply.github.com>
2020-12-02 15:40:31 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 import os
 import sys
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+bot = telebot.TeleBot(os.environ['BOT_KEY'])
+TO_CHAT_ID = os.environ['CHAT_ID']   
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    print(message.chat.id)
+
+@bot.message_handler(commands=['glaza'])
+def glaza(message):
+    bot.send_message(message.chat.id,os.environ['TEXT'])
+    print(message.chat.id)
+    print(message.from_user.id)
+
+
+@bot.message_handler(content_types=['text', 'video', 'photo'])
+def send_text(message):
+    print(message.from_user.id)
+    print(message.text)
+
+    bot.forward_message(TO_CHAT_ID, message.chat.id, message.message_id)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    bot.polling(none_stop=True)
