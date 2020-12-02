547e13be03f998d8dd3121bb3f58272ab82633be
Zerosasha <75369400+Zerosasha@users.noreply.github.com>
2020-12-02 18:58:26 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,9 +3,14 @@ import os
 import sys
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 bot = telebot.TeleBot(os.environ['BOT_KEY'])
 TO_CHAT_ID = os.environ['CHAT_ID']   
+
+@sched.scheduled_job('interval', seconds=30)
+def timed_job():
+    bot.polling()
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -24,5 +29,5 @@ def send_text(message):
     print(message.text)
 
     bot.forward_message(TO_CHAT_ID, message.chat.id, message.message_id)
-bot.polling(none_stop=True)
+
 
